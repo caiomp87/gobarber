5b1eb093c68df5597e04bc792a36aee98054aa61
unknown <Administrator@TRJ3573393.wks.internal.timbrasil.com.br>
2020-02-21 15:55:49 -0300

    validações de agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,6 +1,8 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
+import { is } from 'date-fns/locale';
 
 class AppointmentController {
   async store(req, res) {
@@ -15,6 +17,7 @@ class AppointmentController {
 
     const { provider_id, date } = req.body;
 
+    // Verificar se o agendamento está sendo feito para um prestador
     const isProvider = await User.findOne({
       where: {
         id: provider_id, provider: true }
@@ -24,10 +27,30 @@ class AppointmentController {
       return res.status(401).json({error: 'You can only create appointments with providers' });
     }
 
+    // Verificar se a data de agendamento é uma data passada
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    // Verifica se a data está disponível para agendamento
+    const notAvailable = await Appointment.findOne({
+      where: {
+        provider_id,
+        date: hourStart,
+        cancelled_at: null
+      }
+    });
+
+    if (notAvailable) {
+      return res.status(400).json({ error: 'Appointment dote is not available' });
+    }
+
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date
+      date: hourStart
     });
 
     return res.json(appointment);
