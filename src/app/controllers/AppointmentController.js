3667f85d78a94d39ae0383bafe9ed28798c7579c
unknown <Administrator@TRJ3573393.wks.internal.timbrasil.com.br>
2020-02-22 14:54:49 -0300

    Configurando envio de email caso haja cancelamento de horario.

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,6 +6,8 @@ import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
 
+import Mail from '../../lib/Mail';
+
 class AppointmentController {
   async index(req, res) {
     const { page = 1 } = req.query;
@@ -109,7 +111,20 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email']
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name']
+        }
+      ]
+    });
 
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({ error: "You don't hava permission to cancel this appointment." });
@@ -123,6 +138,21 @@ class AppointmentController {
 
     appointment.cancelled_at = new Date();
     await appointment.save();
+
+    Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(
+          appointment.date,
+          "dd 'de' MMMM, 'às' HH:mm'h'",
+          { locale: pt }
+        )
+      }
+    })
 
     return res.json(appointment);
   }
